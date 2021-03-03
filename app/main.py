33de61281e0d5c46ec0eeecce6480004f404eb2d
adesai <adesai@cloudera.com>
2021-03-03 15:18:40 +0530

    updating to v3

--- app/main.py
+++ app/main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello from Python! This is version 2"
 
+@app.route("/get")
+def getDetails():
+	return "This is a set of details we would like you to have"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
